08191f021322abdd8b08845d7c3c7453461237c5
nowChae <nowchae23@gmail.com>
2024-04-23 19:29:02 +0900

    chore: Create login.index html for kakao login and set .gitignore

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import * as express from 'express';
+import { join } from 'path';
 
 async function bootstrap() {
   dotenv.config();
 
   const app = await NestFactory.create(AppModule);
+
+  app.use(express.static(join(__dirname, '..', 'static')));
+
   const PORT = process.env.PORT;
   await app.listen(PORT);
 }
