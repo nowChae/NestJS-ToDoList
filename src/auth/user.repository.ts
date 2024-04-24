468c7750e093c0665ab4530cc30a2715e013306f
nowChae <nowchae23@gmail.com>
2024-04-24 18:14:53 +0900

    feat: 회원가입 예외 처리 구현

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,7 +1,7 @@
 import { CustomRepository } from 'src/db/typeorm-ex.decorator';
 import { User } from './user.entity';
 import { Repository } from 'typeorm';
-import { NotFoundException } from '@nestjs/common';
+import { ConflictException } from '@nestjs/common';
 
 @CustomRepository(User)
 export class UserRepository extends Repository<User> {
@@ -9,18 +9,26 @@ export class UserRepository extends Repository<User> {
     return this.findOne({ where: { kakaoId } });
   }
 
-  async createUser(user: { kakaoId: string; displayName: string }): Promise<void> {
-    const existingUser = await this.findUserByKakaoId(user.kakaoId);
+  async createUser(user): Promise<void> {
+    try {
+      const existingUser = await this.findUserByKakaoId(user.kakaoId);
 
-    if (!existingUser) {
+      if (existingUser) {
+        throw new ConflictException('이미 존재하는 사용자입니다.');
+      }
+
       const newUser = this.create({
         kakaoId: user.kakaoId,
         displayName: user.displayName,
       });
 
       await this.save(newUser);
-    } else {
-      throw new Error('이미 존재하는 사용자입니다.'); // 이미 존재할 경우 예외 처리
+    } catch (error) {
+      if (error instanceof ConflictException) {
+        throw error; // 이미 존재하는 사용자 예외는 다시 던지기
+      }
+      // 다른 예외인 경우 처리할 수 있음
+      throw new Error('사용자 생성 중에 오류가 발생했습니다.');
     }
   }
 }
